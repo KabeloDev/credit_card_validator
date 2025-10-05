6ddf37352464bccde7852f1948debf13a2ad7b09
Kabelo Makhanya <makhanyak2002@gmail.com>
2025-10-05 16:10:59 +0200

    Add cards form and saved cards displayed in the UI

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,7 @@
 import 'package:credit_card_validator/features/card_validation/data/datasources/local_data_source.dart';
 import 'package:credit_card_validator/features/card_validation/data/repositories/card_repository_impl.dart';
 import 'package:credit_card_validator/features/card_validation/domain/repositories/card_repository.dart';
+import 'package:credit_card_validator/features/card_validation/presentation/blocs/card_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -18,4 +19,6 @@ Future<void> init() async {
   sl.registerLazySingleton<CardRepository>(
     () => CardRepositoryImpl(localDataSource: sl(), prefs: sl()),
   );
+
+  sl.registerFactory(() => CardBloc(repository: sl()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:credit_card_validator/features/card_validation/presentation/blocs/card_bloc.dart';
 import 'package:credit_card_validator/features/card_validation/presentation/main_app_navigation.dart';
 import 'package:credit_card_validator/injection_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,13 +15,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+   return BlocProvider(
+    create: (_) => sl<CardBloc>(), 
+    child: MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MainNavigation(),
-    );
+    ),
+  );
   }
 }
 
