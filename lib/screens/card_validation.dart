f89f8f5bb09687f4a16a051165881232cbdae103
Kabelo Makhanya <makhanyak2002@gmail.com>
2025-10-05 12:55:56 +0200

    Initial card validation screen

---
+++ lib/screens/card_validation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CardValidation extends StatelessWidget {
+  const CardValidation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Card Validation'),
+      ),
+      body: const Center(
+        child: Text('Card Validation Screen'),
+      ),
+    );
+  }
+}
